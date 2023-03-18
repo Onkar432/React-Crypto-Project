2929f1702cb7bff1967013f0a65fd54c5c634e2c
Onkar Pathak <112766269+Onkar432@users.noreply.github.com>
2023-03-18 10:08:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+   <h1>Hello</h1>
+   <Demo/>
+   </>
+  );
+}
+function Demo(){
+  return(
+    <h1>Demo</h1>
+  );
+}
+export default App;
